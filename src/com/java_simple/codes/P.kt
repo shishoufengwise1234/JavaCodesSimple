d0a31a2074bf15673cc764c5eba82d48b6773432
shishoufeng <shishoufeng@szy.cn>
2020-06-15 11:00:42 +0800

    dev:添加构建项目

---
+++ src/com/java_simple/codes/P.kt
@@ -0,0 +1,18 @@
+package com.java_simple.codes
+
+
+/**
+ * Created by shishoufeng on 2020/4/27.
+ *
+ * desc : 日志打印相关
+ *
+ *
+ */
+
+fun out(string: String){
+    println(string)
+}
+
+fun out(any: Any){
+    println(any)
+}
